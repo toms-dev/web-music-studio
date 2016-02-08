df82965aecf6d44d72bc5af3cee6196419f306da
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-02-08 03:47:01 +0100

    Cleaned GUI node_modules

--- audio-core/lib/network/SongAPI.ts
+++ audio-core/lib/network/SongAPI.ts
@@ -5,8 +5,8 @@ import Song from "../Song";
 
 export function createProject(song: Song, callback: (err: any, projectID: number) => void): void {
 	request
-		.post("/api/")
-		.end((err, res) => {
+		.post("/projects/")
+		.end((err: any, res: any) => {
 			console.log("got response!", err, res);
 		})
 }
